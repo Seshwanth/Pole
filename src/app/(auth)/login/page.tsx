b965c7abe28769ca8510c607d9a0f7f8f02d14df
Seshwanth <seshwanth@gmail.com>
2024-11-17 20:54:08 +0530

    Fifth Commit: Adding Logo

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import Button from '@/components/ui/Button'
 import { signIn } from 'next-auth/react'
 import { FC, useState } from 'react'
 import toast from 'react-hot-toast'
+import { Icons } from '@/components/Icons'
 
 
 const Page: FC= ({}) => {
@@ -26,7 +27,7 @@ const Page: FC= ({}) => {
     <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:lx-8'>
         <div className='w-full flex flex-col items-center max-w-md space-y-8'>
             <div className='flex flex-col items-center gap-8'>
-                logo
+              <Icons.Logo className="h-8 w-auto text-indigo-600" />
                 <h2 className='mt-6 text-center font-bold tracking-tight text-grey-900'>
                     Sign in to your account
                 </h2>
